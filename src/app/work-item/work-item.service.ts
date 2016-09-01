0523b02f0b833996b2b5fb02e9d8574dcbe3a8db
Sudipta Sen <sanborn.sen@gmail.com>
2016-09-01 20:35:29 +0530

    Params type need not to be changed from string to number - The params come as string - Database saves the ID as string too - No need to change the type from string to number - And therefore we can use ===

--- src/app/work-item/work-item.service.ts
+++ src/app/work-item/work-item.service.ts
@@ -22,9 +22,9 @@ export class WorkItemService {
       .catch(this.handleError);
   }
 
-  getWorkItem(id: number): Promise<WorkItem> {
+  getWorkItem(id: string): Promise<WorkItem> {
     return this.getWorkItems()
-      .then(workItems => workItems.find(workItem => workItem.id == id));
+      .then(workItems => workItems.find(workItem => workItem.id === id));
   }
 
   delete(workItem: WorkItem): Promise<void> {
