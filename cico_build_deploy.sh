0807eb7886aea440609fac5bab330a1494a07584
Aslak Knutsen <aslak@4fs.no>
2016-09-05 00:30:02 +0200

    Avoid /dist/dist

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -10,7 +10,7 @@ service docker start
 
 # Build builder image
 docker build -t almighty-ui-builder -f Dockerfile.builder .
-docker run --detach=true --name=almighty-ui-builder -e "API_URL=http://demo.api.almighty.io/api/" -t -v $(pwd):/dist:Z almighty-ui-builder
+mkdir dist && docker run --detach=true --name=almighty-ui-builder -e "API_URL=http://demo.api.almighty.io/api/" -t -v $(pwd)/dist:/dist:Z almighty-ui-builder
 
 # Build almigty-ui
 docker exec almighty-ui-builder npm install
@@ -18,7 +18,7 @@ docker exec almighty-ui-builder npm install
 ## some tests...?
 
 docker exec almighty-ui-builder npm run build:prod
-docker exec -u root almighty-ui-builder cp -r /home/almighty/dist /dist
+docker exec -u root almighty-ui-builder cp -r /home/almighty/dist /
 
 ## All ok, deploy
 if [ $? -eq 0 ]; then
--- cico_run_tests.sh
+++ cico_run_tests.sh
@@ -12,7 +12,7 @@ service docker start
 
 # Build builder image
 docker build -t almighty-ui-builder -f Dockerfile.builder .
-docker run --detach=true --name=almighty-ui-builder -e "API_URL=http://demo.api.almighty.io/api/" -t -v $(pwd):/dist:Z almighty-ui-builder
+mkdir dist && docker run --detach=true --name=almighty-ui-builder -e "API_URL=http://demo.api.almighty.io/api/" -t -v $(pwd)/dist:/dist:Z almighty-ui-builder
 
 # Build almigty-ui
 docker exec almighty-ui-builder npm install
@@ -20,4 +20,4 @@ docker exec almighty-ui-builder npm install
 ## some tests...?
 
 docker exec almighty-ui-builder npm run build:prod
-docker exec -u root almighty-ui-builder cp -r /home/almighty/dist /dist
+docker exec -u root almighty-ui-builder cp -r /home/almighty/dist /
