5708edcf09e1e4cdc3b4978dcccb6847dccb3039
Aslak Knutsen <aslak@4fs.no>
2016-09-04 15:58:39 +0200

    Remove docker kill builder iamge. Always run on a clean node

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -9,8 +9,6 @@ sed -i '/OPTIONS=.*/c\OPTIONS="--selinux-enabled --log-driver=journald --insecur
 service docker start
 
 # Build builder image
-docker kill almighty-ui-builder
-
 docker build -t almighty-ui-builder -f Dockerfile.builder .
 docker run --detach=true --name=almighty-ui-builder -e "API_URL=http://demo.api.almighty.io/api/" -t -v $(pwd):/usr/src/app:Z almighty-ui-builder
 
--- cico_run_tests.sh
+++ cico_run_tests.sh
@@ -11,8 +11,6 @@ yum -y install \
 service docker start
 
 # lets test
-docker kill almighty-ui-builder
-
 docker build -t almighty-ui-builder -f Dockerfile.builder .
 docker run --detach=true --name=almighty-ui-builder -e "API_URL=http://demo.api.almighty.io/api/" -t -v $(pwd):/usr/src/app:Z almighty-ui-builder
 
