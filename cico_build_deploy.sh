82b23975b3762d03c5b113d8bfa7aeb0535b44aa
Konrad Kleine <kkleine@redhat.com>
2016-12-01 14:01:23 +0100

    Add codecov and prepared for upload

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -3,6 +3,16 @@
 # Show command before executing
 set -x
 
+# Source environment variables of the jenkins slave
+# that might interest this worker.
+if [ -e "jenkins-env" ]; then
+  cat jenkins-env \
+    | grep -E "(JENKINS_URL|GIT_BRANCH|GIT_COMMIT|BUILD_NUMBER|ghprbSourceBranch|ghprbActualCommit|BUILD_URL|ghprbPullId)=" \
+    | sed 's/^/export /g' \
+    > ~/.jenkins-env
+  source ~/.jenkins-env
+fi
+
 # We need to disable selinux for now, XXX
 /usr/sbin/setenforce 0
 
@@ -33,6 +43,10 @@ docker exec almighty-ui-builder ./run_functional_tests.sh
 
 if [ $? -eq 0 ]; then
   echo 'CICO: functional tests OK'
+
+  # Upload coverage to codecov.io
+  bash <(curl -s https://codecov.io/bash) -t 73933b5a-4aba-4b55-8612-a809ca4ada30 #-X fix
+
   docker exec almighty-ui-builder npm run build:prod
   docker exec -u root almighty-ui-builder cp -r /home/almighty/dist /
   ## All ok, deploy
