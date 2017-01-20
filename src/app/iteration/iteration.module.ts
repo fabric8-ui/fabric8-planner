2fee5d4c25927464cfaba621d4050b327c6920d6
Vikram Raj <vikramraj1991@gmail.com>
2017-01-20 12:00:08 +0530

    Move iteration to module.

---
+++ src/app/iteration/iteration.module.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { CollapseModule } from 'ng2-bootstrap';
+import { DropdownModule } from 'ng2-dropdown';
+
+import { IterationComponent } from './iteration.component';
+import { ModalModule } from 'ng2-modal';
+import { TooltipModule } from 'ng2-bootstrap/components/tooltip';
+
+@NgModule({
+  imports: [
+    DropdownModule,
+    CollapseModule,
+    CommonModule,
+    ModalModule,
+    TooltipModule
+  ],
+  declarations: [IterationComponent],
+  exports: [IterationComponent],
+  providers: []
+})
+export class IterationModule { }
