265be30a71e1490782a3d9fd8b674a11cf7949a7
Vikram Raj <vikramraj1991@gmail.com>
2017-02-03 12:06:02 +0530

    Add toast notification on start iteration.

--- src/app/iteration/iteration-modal/iteration-modal.component.ts
+++ src/app/iteration/iteration-modal/iteration-modal.component.ts
@@ -26,6 +26,8 @@ export class FabPlannerIterationModalComponent implements OnInit, OnChanges {
   private endDate: any;
   private spaceError: Boolean = false;
   private spaceName: string;
+  private isToastNotificationVisible: Boolean = false;
+  private iterationName: string;
 
   private myDatePickerOptions: IMyOptions = {
     dateFormat: 'dd mmm yyyy',
@@ -168,6 +170,10 @@ export class FabPlannerIterationModalComponent implements OnInit, OnChanges {
             this.iterationService.updateIteration(this.iteration)
             .then((iteration) => {
               this.onSubmit.emit(iteration);
+              if(this.modalType == 'start') {
+                this.iterationName = this.iteration.attributes.name;
+                this.isToastNotificationVisible = true;
+              }
               this.resetValues();
               this.createUpdateIterationDialog.close();
             })
@@ -186,6 +192,10 @@ export class FabPlannerIterationModalComponent implements OnInit, OnChanges {
       } else {
         this.validationError = true;
       }
+    }
+
+    closeToastNotification() {
+      this.isToastNotificationVisible = false;
     }
 
     removeError() {
