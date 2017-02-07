d2b0dc97707b13dc726afb9853116b4f75fca8c7
Soumya Deb <debloper@gmail.com>
2017-02-07 20:06:43 +0530

    Fetch and use iterations in list view using the service

--- src/app/work-item/work-item-iteration-association-modal/work-item-iteration-association-modal.component.ts
+++ src/app/work-item/work-item-iteration-association-modal/work-item-iteration-association-modal.component.ts
@@ -1,5 +1,14 @@
 import { Component, ViewChild, OnInit, Output, EventEmitter, Input, OnChanges } from '@angular/core';
 
+import { AuthenticationService } from '../../auth/authentication.service';
+import { Broadcaster } from '../../shared/broadcaster.service';
+import { Logger } from '../../shared/logger.service';
+
+import { WorkItem }          from '../../models/work-item';
+import { WorkItemService }   from '../work-item.service';
+import { IterationModel }    from '../../models/iteration.model';
+import { IterationService }  from '../../iteration/iteration.service';
+
 
 @Component({
   selector: 'fab-planner-associate-iteration-modal',
@@ -10,15 +19,25 @@ export class FabPlannerAssociateIterationModalComponent implements OnInit, OnCha
 
   @ViewChild('iterationAssociationModal') iterationAssociationModal: any;
 
-  ngOnInit() {
+  iterations: IterationModel[] = [];
 
+  constructor(
+    private auth: AuthenticationService,
+    private broadcaster: Broadcaster,
+    private workItemService: WorkItemService,
+    private logger: Logger,
+    private iterationService: IterationService,
+  ) {}
+
+  ngOnInit() {
+    this.iterations = this.iterationService.iterations;
   }
 
   ngOnChanges() {
 
   }
 
-  openAssociateIterationModal() {
+  open() {
     this.iterationAssociationModal.open();
   }
 }
