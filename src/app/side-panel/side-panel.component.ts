7e00fccfda2494a7d1f06c46e18ee82b70f8c860
mitchharpur <mitchharpur@me.com>
2017-02-22 22:10:19 -0500

    correct and modify styles for platform ui part-1 (#1001)

--- src/app/side-panel/side-panel.component.ts
+++ src/app/side-panel/side-panel.component.ts
@@ -3,6 +3,9 @@ import { Router }            from '@angular/router';
 
 
 @Component({
+  host:{
+      'class':"app-component flex-container in-column-direction flex-grow-1"
+  },
   selector: 'side-panel',
   templateUrl: './side-panel.component.html',
   styleUrls: ['./side-panel.component.scss']
