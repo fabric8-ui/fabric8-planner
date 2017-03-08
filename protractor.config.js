4f2a454ee5cb57d04e3a802aa389525321409e65
nainav <naina.verma7@gmail.com>
2017-03-08 19:39:32 +0530

    associating  iteration test

--- protractor.config.js
+++ protractor.config.js
@@ -2,7 +2,7 @@ exports.config = {
     useAllAngular2AppRoots: true,
     getPageTimeout: 30000,
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['src/tests/**/*.spec.js'],
+    specs: ['src/tests/**/*iteration.spec.js'],
     exclude: ['src/tests/**/TBD/*.spec.js', 'src/tests/**/orderOfExecution.spec.js', 'src/tests/**/work-item-timeStamp.spec.js', 'src/tests/**/testHelpers*.spec.js'],
     suites: {
     smokeTest: 'src/tests/**/smokeTest.spec.js'
@@ -13,11 +13,11 @@ exports.config = {
 
     capabilities: {
           'browserName': 'chrome',
-//         'browserName': 'phantomjs',
+        //   'browserName': 'phantomjs',
         //   'maxInstances': 2,
         //   'shardTestFiles': true
-//         'phantomjs.binary.path': require('phantomjs-prebuilt').path,
-//         'phantomjs.cli.args': ['--webdriver-loglevel=ERROR', '--local-storage-path=/tmp/phantom_' + Math.random()]
+        //   'phantomjs.binary.path': require('phantomjs-prebuilt').path,
+        //   'phantomjs.cli.args': ['--webdriver-loglevel=ERROR', '--local-storage-path=/tmp/phantom_' + Math.random()]
 
     }
 };
