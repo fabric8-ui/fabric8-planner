2b35d4204023e94ded6e106a823fe78556b18ecb
Sanborn <sanborn.sen@gmail.com>
2017-03-28 18:37:29 +0530

    fix(detail): Fixed link resolver on update an item

--- src/app/work-item/work-item-detail/work-item-detail.component.ts
+++ src/app/work-item/work-item-detail/work-item-detail.component.ts
@@ -514,10 +514,11 @@ export class WorkItemDetailComponent implements OnInit, AfterViewInit {
           this.areaService.getArea(workItem.relationships.area),
           this.iterationService.getIteration(workItem.relationships.iteration),
           this.workItemService.resolveAssignees(workItem.relationships.assignees),
-          this.workItemService.resolveCreator2(workItem.relationships.creator)
+          this.workItemService.resolveCreator2(workItem.relationships.creator),
+          this.workItemService.resolveLinks(workItem.links.self + '/relationships/links')
         );
       })
-      .subscribe(([workItem, workItemTypes, area, iteration, assignees, creator]) => {
+      .subscribe(([workItem, workItemTypes, area, iteration, assignees, creator, [links, includes]]) => {
         // Resolve area
         workItem.relationships.area = {
           data: area
@@ -542,6 +543,20 @@ export class WorkItemDetailComponent implements OnInit, AfterViewInit {
         workItem.relationships.creator = {
           data: creator
         };
+
+        // Resolve links
+        workItem = Object.assign(
+          workItem,
+          {
+            relationalData: {
+              linkDicts: [],
+              totalLinkCount: 0
+            }
+          }
+        );
+        links.forEach((link) => {
+          this.workItemService.addLinkToWorkItem(link, includes, workItem);
+        });
 
         this.workItem = workItem;
         this.workItemPayload.attributes['version'] = workItem.attributes['version'];
