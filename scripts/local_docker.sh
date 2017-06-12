b48ba161aba3bd9abc11aad3ff6b547d5665597f
Hector Fernandez <hfernand@redhat.com>
2017-06-12 15:37:00 +0200

    Fixed dockerfile and updated runtime/README

--- scripts/local_docker.sh
+++ scripts/local_docker.sh
@@ -1,5 +1,5 @@
 docker build -t almighty-ui-builder -f ./deploy/Dockerfile.builder .
-mkdir -p dist && docker run --detach=true --name=almighty-ui-builder -e "API_URL=http://demo.api.almighty.io/api/" -t -v $(pwd)/dist:/dist:Z almighty-ui-builder
+mkdir -p runtime/dist && docker run --detach=true --name=almighty-ui-builder -e "API_URL=http://demo.api.almighty.io/api/" -t -v ./runtime/dist:/dist:Z almighty-ui-builder
 docker exec almighty-ui-builder npm install
 docker exec almighty-ui-builder ./run_unit_tests.sh
 docker exec -u root almighty-ui-builder cp -r /home/almighty/dist /
