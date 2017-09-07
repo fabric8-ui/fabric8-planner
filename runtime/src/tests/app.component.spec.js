b0a20daa6ae2e9b228c03d459ec00ab838a0180d
rgarg <rgarg@redhat.com>
2017-09-07 11:46:24 +0530

    test(fix):refactor tests

--- runtime/src/tests/app.component.spec.js
+++ runtime/src/tests/app.component.spec.js
@@ -1,10 +1,10 @@
 describe('Splash page', function() {
 
   beforeEach(function() {
-      browser.get('http://localhost:8088/');
+      browser.get('https://openshift.io/rgarg-osiotest1/rgarg-osiotest1-sep4space1/plan');
   });
 
   it('should redirect to the worklist.', function() {
-      expect(browser.getCurrentUrl()).toMatch("http://localhost:8088/");
+      expect(browser.getCurrentUrl()).toMatch("https://openshift.io/rgarg-osiotest1/rgarg-osiotest1-sep4space1/plan");
   });
 });
