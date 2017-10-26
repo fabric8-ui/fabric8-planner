c4793517d1360410946c5384a2b8762c034ef9f5
Divyanshi Gupta <divgupta@localhost.localdomain>
2017-10-26 15:28:14 +0530

    fix(DetailView/QuickPreview) : Clicking outside Assignees popup box closes it

--- src/app/directives/clickoutside.directive.ts
+++ src/app/directives/clickoutside.directive.ts
@@ -11,10 +11,10 @@ export class ClickOutsideDirective {
    @Output()
    public clickOutsidePopup = new EventEmitter();
 
-   @HostListener('document:click', ['$event.target'])
-   public onClick(targetElement) {
+   @HostListener('document:click', ['$event.target','$event.target.classList.contains('+'"assigned_user"'+')'])
+   public onClick(targetElement,assigned_user) {
        const clickedInsidePopup = this.eref.nativeElement.contains(targetElement);
-       const assigned_user = targetElement.classList.contains('assigned_user');
+       //const assigned_user = targetElement.classList.contains('assigned_user');
        if (!clickedInsidePopup&&!assigned_user) {   
            this.clickOutsidePopup.emit(null);
        }
