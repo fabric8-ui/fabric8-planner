93abc349ec32589dfeb0a6241e2faab18c33cdff
Soumya Deb <debloper@gmail.com>
2017-10-30 16:36:26 +0530

    refactor(build): update some strategies about how the integrated build works
    - allow user to provide repo paths or clone from source
    - clean up intermediate containers upter the build
    - use /tmp/<repo> as the path to clone repo
    - remove image/containers with same tag before building new to avoid issues
    - fix build artifact copy over from container caused by volume unlining

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,21 +1,74 @@
 #!/bin/bash
 set -x
 
-echo "Setting up paths and working directories..."
+echo "Setting up working directories..."
 BASEPATH=`pwd -P`
-PLANNERPATH="$BASEPATH/planner"
-PLATFORMPATH="$BASEPATH/platform"
+PLANNERPATH=""
+PLATFORMPATH=""
 
-# echo "Setting up some environment variables, which ideally should be set in app as defaults..."
-# export FABRIC8_WIT_API_URL="https://api.prod-preview.openshift.io/api/"
-# export FABRIC8_RECOMMENDER_API_URL="https://api-bayesian.dev.rdu2c.fabric8.io/api/v1/"
-# export FABRIC8_FORGE_API_URL="https://forge.api.prod-preview.openshift.io"
-# export FABRIC8_SSO_API_URL="https://sso.prod-preview.openshift.io/"
-# export FABRIC8_REALM="fabric8-test"
+echo "Parsing CLI arguments..."
+for i in "$@"
+do
+case $i in
+    --planner=* )
+        if [[ -d "${i#*=}" ]]; then
+            # Provided path _is_ indeed a directory
+            cd ${i#*=}
+            # Let's validate if it contains the Planner source code
+            if [[ $(npm view . name) = fabric8-planner ]]; then
+                # The path seems to contain package.json which reports to be Planner source
+                PLANNERPATH=${i#*=}
+                echo "Planner path is set to: $PLANNERPATH"
+            else
+                echo -e "\e[1;31mCan't resolve source at provided Planner path\e[0m"
+                exit 1
+            fi
+        else
+            echo -e "\e[1;31mProvided Planner path is invalid\e[0m"
+            exit 1
+        fi
+        shift;;
+    --platform=* )
+        if [[ -d "${i#*=}" ]]; then
+            # Provided path _is_ indeed a directory
+            cd ${i#*=}
+            # Let's validate if it contains the Platform source code
+            if [[ $(npm view . name) = fabric8-ui ]]; then
+                # The path seems to contain package.json which reports to be Platform source
+                PLATFORMPATH=${i#*=}
+                echo "Platform path is set to: $PLATFORMPATH"
+            else
+                echo -e "\e[1;31mCan't resolve source at provided Platform path\e[0m"
+                exit 1
+            fi
+        else
+            echo -e "\e[1;31mProvided Platform path is invalid\e[0m"
+            exit 1
+        fi
+        shift;;
+    * )
+        ;;
+esac
+done
 
-echo "Downloading sources..."
-git clone https://github.com/fabric8-ui/fabric8-planner.git planner
-git clone https://github.com/fabric8-ui/fabric8-ui.git platform
+echo "Validating repositories..."
+if [[ -z $PLANNERPATH ]]; then
+    echo "Local Planner repo path wasn't provided, cloning from upstream to '/tmp/planner':"
+    PLANNERPATH=/tmp/planner
+    git clone https://github.com/fabric8-ui/fabric8-planner.git $PLANNERPATH
+fi
+if [[ -z $PLATFORMPATH ]]; then
+    echo "Local Platform repo path wasn't provided, cloning from upstream to '/tmp/platform':"
+    PLATFORMPATH=/tmp/platform
+    git clone https://github.com/fabric8-ui/fabric8-ui.git $PLATFORMPATH
+fi
+
+echo "Setting up some environment variables, which ideally should be set in app as defaults..."
+export FABRIC8_WIT_API_URL="https://api.prod-preview.openshift.io/api/"
+export FABRIC8_RECOMMENDER_API_URL="https://api-bayesian.dev.rdu2c.fabric8.io/api/v1/"
+export FABRIC8_FORGE_API_URL="https://forge.api.prod-preview.openshift.io"
+export FABRIC8_SSO_API_URL="https://sso.prod-preview.openshift.io/"
+export FABRIC8_REALM="fabric8-test"
 
 echo "Installing global prerequisites..."
 npm install -g rimraf gulp webpack webpack-merge
@@ -30,11 +83,12 @@ service docker start
 
 echo "Switching over to f8ui..."
 cd $PLATFORMPATH
-mkdir -p dist
 
 echo "Making f8ui-builder image and starting the container..."
+docker stop fabric8-ui-builder && docker rm fabric8-ui-builder
+docker rmi fabric8-ui-builder
 docker build -t fabric8-ui-builder -f Dockerfile.builder .
-docker run -d --name=fabric8-ui-builder -t -v $(pwd)/dist:/dist -v $(pwd)/../planner/dist:/planner -e FABRIC8_BRANDING=openshiftio -e FABRIC8_REALM=fabric8 fabric8-ui-builder
+docker run -d --name=fabric8-ui-builder -t -v $PLATFORMPATH/dist:/dist -v $PLANNERPATH/dist:/planner fabric8-ui-builder
 
 echo "Linking planner to platform within f8ui build container..."
 docker exec -u root fabric8-ui-builder npm install
@@ -43,14 +97,21 @@ docker exec -u root fabric8-ui-builder npm link /planner
 echo "Building platform with integrated planner..."
 docker exec -u root fabric8-ui-builder npm run build:prod
 
-# echo "Copy over the build artifacts from container to host..."
-# echo ""
-# echo "This is only needed if `npm run clean` was performed,"
-# echo "which removes the `dist` directory from host, unlinking the volume"
-# docker exec -u root fabric8-ui-builder cp -r ./dist /
+echo "Copy over the build artifacts from container to host..."
+echo ""
+echo "This is only needed because the platform build executes `npm run clean`"
+echo "which removes the `dist` directory from the host, unlinking the volume"
+docker cp fabric8-ui-builder:/home/fabric8/fabric8-ui/dist /tmp/platform/
 
 echo "Generating f8ui integrated planner image from build artifacts..."
+docker rm fabric8-planner-platform
+docker rmi fabric8-planner-platform
 docker build -t fabric8-planner-platform .
 
-# echo "Run the container, and visit http://localhost:8080/ on host browser..."
-# docker run -it --user=root -p 8080:80 --name=fabric8-planner-platform fabric8-planner-platform
+echo "Cleaning up intermediate build container and image..."
+docker stop fabric8-ui-builder
+docker rm fabric8-ui-builder
+docker rmi fabric8-ui-builder
+
+echo "Running the container; visit http://localhost:8080/ on host browser..."
+docker run -it --user=root -p 8080:80 --name=fabric8-planner-platform fabric8-planner-platform
