4a3d99a5516f0dad9f95a341c437f79ba5a110b8
Nimisha Mukherjee <nimishamukerjee@gmail.com>
2017-11-10 18:36:42 +0530

    fix(list-view): Remove duplicate child work items that appear as a root and under the parent.

--- src/app/components/planner-list/planner-list.component.ts
+++ src/app/components/planner-list/planner-list.component.ts
@@ -465,9 +465,20 @@ export class PlannerListComponent implements OnInit, AfterViewInit, DoCheck, OnD
       console.log('Performance :: Fetching the initial list - '  + (t2 - t1) + ' milliseconds.');
       this.logger.log('Got work item list.');
       this.logger.log(workItemResp.workItems);
-      const workItems = workItemResp.workItems;
+      const workItemsAll = workItemResp.workItems;
       this.nextLink = workItemResp.nextLink;
       this.included = workItemResp.included;
+      //Remove work item duplicates - a child work item
+      //should not appear part of the root response
+      const workItems = workItemsAll.filter( wi => {
+        if( wi.relationships.parent.data != undefined ) {
+          //take the parent ID and loop thorough the work items
+          if (workItemsAll.findIndex(item => item.id === wi.relationships.parent.data.id) === -1)
+            return wi;
+        } else {
+          return wi;
+        }
+      });
       this.workItems = this.workItemService.resolveWorkItems(
         workItems,
         this.iterations,
