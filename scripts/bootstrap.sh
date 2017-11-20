bb922045817e158bf7b124a13091baba5bc3faa4
Soumya Deb <debloper@gmail.com>
2017-11-20 17:21:29 +0530

    feat(orchestration): add support for `--dryrun` and skip usage of builder image
    - skip build steps for both library and f8ui, creates a mock index.html
    - determine and separate build issues from orchestration issues
    - emulate release.groovy's orchestration steps (on-host build)

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,6 +5,7 @@ echo "Setting up working directories..."
 BASEPATH=`pwd -P`
 PLANNERPATH=""
 PLATFORMPATH=""
+DRYRUN=false
 
 echo "Parsing CLI arguments..."
 for i in "$@"
@@ -46,6 +47,9 @@ case $i in
             exit 1
         fi
         shift;;
+    --dryrun )
+        DRYRUN=true
+        shift;;
     * )
         ;;
 esac
@@ -73,10 +77,17 @@ export FABRIC8_REALM="fabric8-test"
 echo "Installing global prerequisites..."
 npm install -g rimraf gulp webpack webpack-merge
 
-echo "Building planner library..."
 cd $PLANNERPATH
-npm install
-npm run build:library
+if [[ $DRYRUN = true ]]
+then
+    echo "Dry running mock build (bypasses actual build step)..."
+    mkdir -p dist
+    cp package.json dist/
+else
+    echo "Building planner library..."
+    npm install
+    npm run build:library
+fi
 
 echo "Starting docker..."
 service docker start
@@ -84,34 +95,56 @@ service docker start
 echo "Switching over to f8ui..."
 cd $PLATFORMPATH
 
-echo "Making f8ui-builder image and starting the container..."
-docker stop fabric8-ui-builder && docker rm fabric8-ui-builder
-docker rmi fabric8-ui-builder
-docker build -t fabric8-ui-builder -f Dockerfile.builder .
-docker run -d --name=fabric8-ui-builder -t -v $PLATFORMPATH/dist:/dist -v $PLANNERPATH/dist:/planner fabric8-ui-builder
+# echo "Making f8ui-builder image and starting the container..."
+# docker stop fabric8-ui-builder && docker rm fabric8-ui-builder
+# docker rmi fabric8-ui-builder
+# docker build -t fabric8-ui-builder -f Dockerfile.builder .
+# docker run -d --name=fabric8-ui-builder -t -v $PLATFORMPATH/dist:/dist -v $PLANNERPATH/dist:/planner fabric8-ui-builder
 
-echo "Linking planner to platform within f8ui build container..."
-docker exec -u root fabric8-ui-builder npm install
-docker exec -u root fabric8-ui-builder npm link /planner
 
-echo "Building platform with integrated planner..."
-docker exec -u root fabric8-ui-builder npm run build:prod
+# if [[ $DRYRUN = true ]]
+# then
+#     echo "Dry running mock build (bypasses actual build step)..."
+#     docker exec -u root fabric8-ui-builder echo 'Mock Build' > ./dist/index.html
+# else
+#     echo "Linking planner to platform within f8ui build container..."
+#     docker exec -u root fabric8-ui-builder npm install
+#     docker exec -u root fabric8-ui-builder npm link /planner
 
-echo "Copy over the build artifacts from container to host..."
-echo ""
-echo "This is only needed because the platform build executes 'npm run clean'"
-echo "which removes the 'dist' directory from the host, unlinking the volume"
-docker cp fabric8-ui-builder:/home/fabric8/fabric8-ui/dist $PLATFORMPATH/
+#     echo "Building platform with integrated planner..."
+#     docker exec -u root fabric8-ui-builder npm run build:prod
+# fi
+
+# echo "Copy over the build artifacts from container to host..."
+# echo ""
+# echo "This is only needed because the platform build executes 'npm run clean'"
+# echo "which removes the 'dist' directory from the host, unlinking the volume"
+# docker cp fabric8-ui-builder:/home/fabric8/fabric8-ui/dist $PLATFORMPATH/
+
+echo "Alternate build step, without using builder image, similar to f8ui/release.groovy..."
+if [[ $DRYRUN = true ]]
+then
+    echo "Dry running mock build (bypasses actual build step)..."
+    mkdir -p dist
+    echo 'Mock Build' > ./dist/index.html
+else
+    echo "Linking planner to platform within f8ui build container..."
+    npm install
+    npm link $PLANNERPATH/dist
+
+    echo "Building platform with integrated planner..."
+    npm run build:prod
+fi
 
 echo "Generating f8ui integrated planner image from build artifacts..."
 docker rm fabric8-planner-platform
 docker rmi fabric8-planner-platform
-docker build -t fabric8-planner-platform -f Dockerfile.deploy .
+docker build -t fabric8-planner-platform -f Dockerfile .
 
-echo "Cleaning up intermediate build container and image..."
-docker stop fabric8-ui-builder
-docker rm fabric8-ui-builder
-docker rmi fabric8-ui-builder
+# echo "Cleaning up intermediate build container and image..."
+# docker stop fabric8-ui-builder
+# docker rm fabric8-ui-builder
+# docker rmi fabric8-ui-builder
 
 # echo "Running the container; visit http://localhost:8080/ on host browser..."
 # docker run -it --user=root -p 8080:80 --name=fabric8-planner-platform fabric8-planner-platform
