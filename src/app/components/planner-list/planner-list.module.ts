ed271ffc6a7fa84877e67beb70df68b9ae9d1ba4
Pranav Gore <pranavgore09@gmail.com>
2017-12-14 10:47:05 +0530

    introduce an error to make fabric8-ui build fail

--- src/app/components/planner-list/planner-list.module.ts
+++ src/app/components/planner-list/planner-list.module.ts
@@ -20,6 +20,7 @@ import {
   WidgetsModule
 } from 'ngx-widgets';
 
+import { NgxDatatableModule } from '../../../../node_modules/rh-ngx-datatable';
 import { ActionModule, ListModule } from 'patternfly-ng';
 import { Logger } from 'ngx-base';
 import { AuthenticationService } from 'ngx-login-client';
