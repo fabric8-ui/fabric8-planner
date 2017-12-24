7d71da34c39c21055f05913815225ef36714e280
Ibrahim Jarif <jarifibrahim@gmail.com>
2017-12-24 15:48:10 +0530

    fix(script): Remove commented code

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -95,32 +95,6 @@ service docker start
 echo "Switching over to f8ui..."
 cd $PLATFORMPATH
 
-# echo "Making f8ui-builder image and starting the container..."
-# docker stop fabric8-ui-builder && docker rm fabric8-ui-builder
-# docker rmi fabric8-ui-builder
-# docker build -t fabric8-ui-builder -f Dockerfile.builder .
-# docker run -d --name=fabric8-ui-builder -t -v $PLATFORMPATH/dist:/dist -v $PLANNERPATH/dist:/planner fabric8-ui-builder
-
-
-# if [[ $DRYRUN = true ]]
-# then
-#     echo "Dry running mock build (bypasses actual build step)..."
-#     docker exec -u root fabric8-ui-builder echo 'Mock Build' > ./dist/index.html
-# else
-#     echo "Linking planner to platform within f8ui build container..."
-#     docker exec -u root fabric8-ui-builder npm install
-#     docker exec -u root fabric8-ui-builder npm link /planner
-
-#     echo "Building platform with integrated planner..."
-#     docker exec -u root fabric8-ui-builder npm run build:prod
-# fi
-
-# echo "Copy over the build artifacts from container to host..."
-# echo ""
-# echo "This is only needed because the platform build executes 'npm run clean'"
-# echo "which removes the 'dist' directory from the host, unlinking the volume"
-# docker cp fabric8-ui-builder:/home/fabric8/fabric8-ui/dist $PLATFORMPATH/
-
 echo "Alternate build step, without using builder image, similar to f8ui/release.groovy..."
 if [[ $DRYRUN = true ]]
 then
@@ -140,11 +114,6 @@ echo "Generating f8ui integrated planner image from build artifacts..."
 docker rm fabric8-planner-platform
 docker rmi fabric8-planner-platform
 docker build -t fabric8-planner-platform -f Dockerfile.deploy .
-
-# echo "Cleaning up intermediate build container and image..."
-# docker stop fabric8-ui-builder
-# docker rm fabric8-ui-builder
-# docker rmi fabric8-ui-builder
 
 # echo "Running the container; visit http://localhost:8080/ on host browser..."
 # docker run -it -p 8088:8080 --name=fabric8-planner-platform fabric8-planner-platform
