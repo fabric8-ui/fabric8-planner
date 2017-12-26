e8e437684a2e4c73f370130633844d353c4195e1
Ibrahim Jarif <jarifibrahim@gmail.com>
2017-12-26 13:44:22 +0530

    fix(tests): Port quickadd-workitem tests to the new DB

--- runtime/src/tests/work-item/work-item-list/quickadd-workitem.spec.js
+++ runtime/src/tests/work-item/work-item-list/quickadd-workitem.spec.js
@@ -16,19 +16,39 @@
 
 var WorkItemListPage = require('./page-objects/work-item-list.page'),
   constants = require('./constants'),
-  testSupport = require('./testSupport');
+  testSupport = require('./testSupport'),
+  OpenShiftIoRHDLoginPage = require('./page-objects/openshift-io-RHD-login.page');
 
 describe('Quickadd - Work item list', function () {
-  var page, items, browserMode;
+  var page, AUTH_TOKEN, REFRESH_TOKEN, until = protractor.ExpectedConditions;
 //  var char255 = '<div *ngFor=let comment of workItem.relationalData.comments; let counter = index" class="comments-wrap">   +            <div *ngFor="let comment of workItem.relationalData?.comments?.slice().reverse()" class="comments-wrap">                  <div>                      <div class="user-avatar pull-left">                          <img id="{{"comment_avatar_" + counter}}" -                        class="user-assign-avatar pull-left"  +                               />';
   var char255 = 'abcde1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890';
   var char255Expected = '<div *ngFor=let comment of workItem.relationalData.comments; let counter = index" class="comments-wrap"> + <div *ngFor="let comment of workItem.relationalData?.comments?.slice().reverse()" class="comments-wrap"> <div> <div class="user-avatar pull-left"> <img id="{{"comment_avatar_" + counter}}" - class="user-assign-avatar pull-left" + />';
-  var until = protractor.ExpectedConditions;
 
   beforeEach(function () {
+    browser.ignoreSynchronization = false;
     testSupport.setBrowserMode('desktop');
-    page = new WorkItemListPage(true);
-    testSupport.setTestSpace(page);
+    if (AUTH_TOKEN && REFRESH_TOKEN){
+      console.log("AUTH and REFRESH tokens found. Skipping login.")
+      page = new WorkItemListPage(this.AUTH_TOKEN, this.REFRESH_TOKEN);
+    } else {
+      page = new WorkItemListPage()
+    }
+  });
+
+  /* Simple test for registered user */
+  it("should perform - LOGIN", function() {
+    /* Login to SUT */
+    page.clickLoginButton();
+    browser.ignoreSynchronization = true;
+    var RHDpage = new OpenShiftIoRHDLoginPage();
+    RHDpage.doLogin(browser);
+    browser.executeScript("return window.localStorage.getItem('auth_token');").then(function(val) {
+      this.AUTH_TOKEN = val;
+    });
+    browser.executeScript("return window.localStorage.getItem('refresh_token');").then(function(val) {
+      this.REFRESH_TOKEN = val
+    });
   });
 
 // March 28 - Failing due to: https://github.com/fabric8-ui/fabric8-planner/issues/1348
