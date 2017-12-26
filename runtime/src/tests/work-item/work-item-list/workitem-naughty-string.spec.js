a61380a0a1d1a5ec93ecd8684ac94fc1db3365f3
Ibrahim Jarif <jarifibrahim@gmail.com>
2017-12-26 15:54:29 +0530

    fix(tests): Port workitem-naughty-string tests to the new DB

--- runtime/src/tests/work-item/work-item-list/workitem-naughty-string.spec.js
+++ runtime/src/tests/work-item/work-item-list/workitem-naughty-string.spec.js
@@ -15,74 +15,93 @@
  */
 
 var WorkItemListPage = require('./page-objects/work-item-list.page'),
-  testSupport = require('./testSupport');
+  testSupport = require('./testSupport'),
+  OpenShiftIoRHDLoginPage = require('./page-objects/openshift-io-RHD-login.page');
 
 describe('Work item list', function () {
-  var page, items, browserMode;
-  var until = protractor.ExpectedConditions;
+  var page, AUTH_TOKEN, REFRESH_TOKEN, until = protractor.ExpectedConditions;
   var waitTime = 30000;
 
-/* Troublesome strings reference: https://github.com/minimaxir/big-list-of-naughty-strings */
-var UnicodeSymbols = "Ω≈ç√∫˜µ≤≥÷åß∂ƒ©˙∆˚¬…æœ∑´®†¥¨ˆøπ“‘¡™£¢∞§¶•ªº–≠¸˛Ç◊ı˜Â¯˘¿ÅÍÎÏ˝ÓÔÒÚÆ☃Œ„´‰ˇÁ¨ˆØ∏”’`⁄€‹›ﬁﬂ‡°·‚—±⅛⅜⅝⅞ЁЂЃЄЅІЇЈЉЊЋЌЍЎЏАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюя٠١٢٣٤٥٦٧٨٩";
-var TwoByteCharacters = "田中さんにあげて下さいパーティーへ行かないか和製漢語部落格사회과학원 어학연구소찦차를 타고 온 펲시맨과 쑛다리 똠방각하社會科學院語學研";
-var JapaneseEmoticons = "ヽ༼ຈل͜ຈ༽ﾉ ヽ༼ຈل͜ຈ༽ﾉ(｡◕ ∀ ◕｡)｀ｨ(´∀｀∩__ﾛ(,_,*)・(￣∀￣)・:*:ﾟ･✿ヾ╲(｡◕‿◕｡)╱✿･ﾟ,。・:*:・゜’( ☻ ω ☻ )。・:*:・゜’(╯°□°）╯︵ ┻━┻)(ﾉಥ益ಥ）ﾉ﻿ ┻━┻┬─┬ノ( º _ ºノ)( ͡° ͜ʖ ͡°)";
-var RightToLeftStrings = "בְּרֵאשִׁית, בָּרָא אֱלֹהִים, אֵת הַשָּׁמַיִם, וְאֵת הָאָרֶץم نفس سقطت وبالتحديد،, جزيرتي باستخدام أن دنو. إذ هنا؟ الستار وتنصيب كان. أهّل ايطاليا، بريطانيا-فرنسا قد أخذ. سليمان، إتفاقية بين ما,";
-var ScriptInjection = "<script>alert(123)</script> &lt;script&gt;alert(&#39;123&#39;);&lt;/script&gt; <img src=x onerror=alert(123) /> <svg><script>123<1>alert(123)</script>";
+  /* Troublesome strings reference: https://github.com/minimaxir/big-list-of-naughty-strings */
+  var UnicodeSymbols = "Ω≈ç√∫˜µ≤≥÷åß∂ƒ©˙∆˚¬…æœ∑´®†¥¨ˆøπ“‘¡™£¢∞§¶•ªº–≠¸˛Ç◊ı˜Â¯˘¿ÅÍÎÏ˝ÓÔÒÚÆ☃Œ„´‰ˇÁ¨ˆØ∏”’`⁄€‹›ﬁﬂ‡°·‚—±⅛⅜⅝⅞ЁЂЃЄЅІЇЈЉЊЋЌЍЎЏАБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюя٠١٢٣٤٥٦٧٨٩";
+  var TwoByteCharacters = "田中さんにあげて下さいパーティーへ行かないか和製漢語部落格사회과학원 어학연구소찦차를 타고 온 펲시맨과 쑛다리 똠방각하社會科學院語學研";
+  var JapaneseEmoticons = "ヽ༼ຈل͜ຈ༽ﾉ ヽ༼ຈل͜ຈ༽ﾉ(｡◕ ∀ ◕｡)｀ｨ(´∀｀∩__ﾛ(,_,*)・(￣∀￣)・:*:ﾟ･✿ヾ╲(｡◕‿◕｡)╱✿･ﾟ,。・:*:・゜’( ☻ ω ☻ )。・:*:・゜’(╯°□°）╯︵ ┻━┻)(ﾉಥ益ಥ）ﾉ﻿ ┻━┻┬─┬ノ( º _ ºノ)( ͡° ͜ʖ ͡°)";
+  var RightToLeftStrings = "בְּרֵאשִׁית, בָּרָא אֱלֹהִים, אֵת הַשָּׁמַיִם, וְאֵת הָאָרֶץم نفس سقطت وبالتحديد،, جزيرتي باستخدام أن دنو. إذ هنا؟ الستار وتنصيب كان. أهّل ايطاليا، بريطانيا-فرنسا قد أخذ. سليمان، إتفاقية بين ما,";
+  var ScriptInjection = "<script>alert(123)</script> &lt;script&gt;alert(&#39;123&#39;);&lt;/script&gt; <img src=x onerror=alert(123) /> <svg><script>123<1>alert(123)</script>";
 
-/* Protractor and/or Jasmine and/or Javascript is unable to handle these strings */
-var UnicodeSubscriptSuperscriptAccents = "⁰⁴⁵₀₁₂⁰⁴⁵₀₁₂";
-var Emoji = "😍❤️ 💔 💕 💓 💗 💖 💘 0️⃣ 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ 6️⃣ 7️⃣ 8️⃣ 9️⃣";
-var UnicodeFont = "Ｔｈｅ ｑｕｉｃｋ ｂｒｏｗｎ 𝐟𝐨𝐱 𝐣𝐮𝐦𝐩𝐬 𝐨𝐯𝐞𝐫  𝖙𝖍𝖊 𝖑𝖆𝖟𝖞 𝖉𝖔𝖌";
-var UnicodeNumbers = "１２３١٢٣";
+  /* Protractor and/or Jasmine and/or Javascript is unable to handle these strings */
+  var UnicodeSubscriptSuperscriptAccents = "⁰⁴⁵₀₁₂⁰⁴⁵₀₁₂";
+  var Emoji = "😍❤️ 💔 💕 💓 💗 💖 💘 0️⃣ 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ 6️⃣ 7️⃣ 8️⃣ 9️⃣";
+  var UnicodeFont = "Ｔｈｅ ｑｕｉｃｋ ｂｒｏｗｎ 𝐟𝐨𝐱 𝐣𝐮𝐦𝐩𝐬 𝐨𝐯𝐞𝐫  𝖙𝖍𝖊 𝖑𝖆𝖟𝖞 𝖉𝖔𝖌";
+  var UnicodeNumbers = "１２３١٢٣";
 
   beforeEach(function () {
+    browser.ignoreSynchronization = false;
     testSupport.setBrowserMode('desktop');
-    // browser.ignoreSynchronization = true;
-    page = new WorkItemListPage(true);
-    testSupport.setTestSpace(page);
+    if (AUTH_TOKEN && REFRESH_TOKEN){
+      console.log("AUTH and REFRESH tokens found. Skipping login.")
+      page = new WorkItemListPage(this.AUTH_TOKEN, this.REFRESH_TOKEN);
+    } else {
+      page = new WorkItemListPage()
+    }
   });
 
-it('Quick create UnicodeSymbols workitems', function () { quickCreateWorkItem (UnicodeSymbols); });
-it('Quick create TwoByteCharacters workitems', function () { quickCreateWorkItem (TwoByteCharacters); });
-it('Quick create JapaneseEmoticons workitems', function () { quickCreateWorkItem (JapaneseEmoticons); });
-it('Quick create RightToLeftStrings workitems', function () { quickCreateWorkItem (RightToLeftStrings); });
-
-/* Commented out due to how the Planner UI interprets <> chars - this is a separate Planner UI bug */
-//it('Quick create ScriptInjection workitems', function () { quickCreateWorkItem (ScriptInjection); });
-
-it('Create UnicodeSymbols workitems', function () { createWorkItem (UnicodeSymbols); });
-it('Create TwoByteCharacters workitems', function () { createWorkItem (TwoByteCharacters); });
-it('Create JapaneseEmoticons workitems', function () { createWorkItem (JapaneseEmoticons); });
-it('Create RightToLeftStrings workitems', function () { createWorkItem (RightToLeftStrings); });
-it('Create ScriptInjection workitems', function () { createWorkItem (ScriptInjection); });
-
-/* Quick create a workitem */
-var quickCreateWorkItem = function(theText) {
-    page.clickWorkItemQuickAdd();
-    page.typeQuickAddWorkItemTitle(theText);
-    page.clickQuickAddSave().then(function() {
-      expect(page.workItemTitle(page.firstWorkItem)).toBe(theText);
-      expect(page.workItemTitle(page.workItemByNumber(0))).toBe(theText);
+  /* Simple test for registered user */
+  it("should perform - LOGIN", function() {
+    /* Login to SUT */
+    page.clickLoginButton();
+    browser.ignoreSynchronization = true;
+    var RHDpage = new OpenShiftIoRHDLoginPage();
+    RHDpage.doLogin(browser);
+    browser.executeScript("return window.localStorage.getItem('auth_token');").then(function(val) {
+      this.AUTH_TOKEN = val;
     });
-}
+    browser.executeScript("return window.localStorage.getItem('refresh_token');").then(function(val) {
+      this.REFRESH_TOKEN = val
+    });
+  });
 
-/* Create a workitem */
-var createWorkItem = function(theText) {
+  it('Quick create UnicodeSymbols workitems', function () { quickCreateWorkItem (UnicodeSymbols); });
+  it('Quick create TwoByteCharacters workitems', function () { quickCreateWorkItem (TwoByteCharacters); });
+  it('Quick create JapaneseEmoticons workitems', function () { quickCreateWorkItem (JapaneseEmoticons); });
+  it('Quick create RightToLeftStrings workitems', function () { quickCreateWorkItem (RightToLeftStrings); });
 
-    testSupport.setBrowserMode('desktop');
-    //console.log (theText);
+  /* Commented out due to how the Planner UI interprets <> chars - this is a separate Planner UI bug */
+  //it('Quick create ScriptInjection workitems', function () { quickCreateWorkItem (ScriptInjection); });
 
-   page.clickDetailedDialogButton();
-   var detailPage = page.clickDetailedIcon("userstory");
+  it('Create UnicodeSymbols workitems', function () { createWorkItem (UnicodeSymbols); });
+  it('Create TwoByteCharacters workitems', function () { createWorkItem (TwoByteCharacters); });
+  it('Create JapaneseEmoticons workitems', function () { createWorkItem (JapaneseEmoticons); });
+  it('Create RightToLeftStrings workitems', function () { createWorkItem (RightToLeftStrings); });
+  it('Create ScriptInjection workitems', function () { createWorkItem (ScriptInjection); });
 
-   detailPage.clickWorkItemDetailTitle2();
-   browser.wait(until.visibilityOf(detailPage.workItemDetailTitle), waitTime, 'Failed to find workItemDetailTitle');
-   detailPage.setWorkItemDetailTitle (theText, false);
+  /* Quick create a workitem */
+  var quickCreateWorkItem = function(theText) {
+      page.clickWorkItemQuickAdd();
+      page.typeQuickAddWorkItemTitle(theText);
+      page.clickQuickAddSave().then(function() {
+        expect(page.workItemTitle(page.firstWorkItem)).toBe(theText);
+        expect(page.workItemTitle(page.workItemByNumber(0))).toBe(theText);
+      });
+  }
 
-   detailPage.clickWorkItemDetailTitle2();
+  /* Create a workitem */
+  var createWorkItem = function(theText) {
 
-   browser.wait(until.visibilityOf(detailPage.workItemTitleSaveIcon), waitTime, 'Failed to find workItemTitleSaveIcon');
-   detailPage.clickWorkItemTitleSaveIcon();
+      testSupport.setBrowserMode('desktop');
+      //console.log (theText);
+
+    page.clickDetailedDialogButton();
+    var detailPage = page.clickDetailedIcon("bug");
+
+    detailPage.clickWorkItemDetailTitle2();
+    browser.wait(until.visibilityOf(detailPage.workItemDetailTitle), waitTime, 'Failed to find workItemDetailTitle');
+    detailPage.setWorkItemDetailTitle (theText, false);
+
+    detailPage.clickWorkItemDetailTitle2();
+
+    browser.wait(until.visibilityOf(detailPage.workItemTitleSaveIcon), waitTime, 'Failed to find workItemTitleSaveIcon');
+    detailPage.clickWorkItemTitleSaveIcon();
 
  /* TODO - Issue with mocking - workitems' descriptions are not persisted in mocking data */ 
 //   detailPage.clickWorkItemDetailDescription();
@@ -97,6 +116,6 @@ var createWorkItem = function(theText) {
     https://openshift.io/openshiftio/openshiftio/plan/detail/1581  */
 //   expect(page.workItemTitle(page.firstWorkItem)).toBe(theText);
 //   expect(page.workItemTitle(page.workItemByNumber(0))).toBe(theText);
-}
+  }
 
 });
