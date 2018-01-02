ebc46e80a7eff4200da9ee5f0e7ea525376ada53
Soumya Deb <debloper@gmail.com>
2018-01-03 02:32:51 +0530

    feat(website): fetch and display top contributors

--- script.js
+++ script.js
@@ -16,13 +16,28 @@ $.get("https://api.github.com/repos/fabric8-ui/fabric8-planner", function (respo
 
 // Populate contributor details
 $.get("https://api.github.com/repos/fabric8-ui/fabric8-planner/contributors", function (contributors) {
-  var contributorList = [];
 
-  for (var contributor of contributors) {
-    contributorList.push(contributor.login)
-  }
+  $(document).ready(function () {
+    if ('content' in document.createElement('template')) {
 
-  $(document).ready(function() {
-    $("#repoContrib").text(contributorList.join(", "));
-  });
-})
+      var contribCard = document.getElementById("contributorCardTemplate");
+
+      var theirPhoto = contribCard.content.querySelector("img"),
+          theirTitle = contribCard.content.querySelector(".card-title a"),
+          theirWorks = contribCard.content.querySelector(".card-subtitle");
+
+      for (var i=0; i<8; i++) {
+        var contributor = contributors[i];
+
+        theirPhoto.src = contributor.avatar_url;
+        theirTitle.href = contributor.html_url;
+        theirTitle.innerText = contributor.login;
+        theirWorks.innerText = "" + contributor.contributions + " Contributions";
+
+        var templateClone = document.importNode(contribCard.content, true);
+        document.getElementById("repoContrib").appendChild(templateClone);
+      }
+    } // @TODO: else {}
+  })
+
+});
