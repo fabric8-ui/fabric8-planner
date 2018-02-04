86a1d7c5aac31bcff662503185af8a5f81bfb65b
Pranav Gore <pranavgore09@gmail.com>
2018-02-04 19:47:51 +0530

    bake ENV into the image

--- deploy/release.groovy
+++ deploy/release.groovy
@@ -54,7 +54,16 @@ def getStandaloneImage(imageName){
             container('ui'){
                sh 'npm cache clean --force'
                sh 'npm install'
-               sh 'npm run build'
+               sh '''
+                export API_URL=https://api.prod-preview.openshift.io/api/
+                export FORGE_URL=https://forge.api.prod-preview.openshift.io/
+                export FABRIC8_REALM=fabric8-test
+                export FABRIC8_WIT_API_URL=https://api.prod-preview.openshift.io/api/
+                export FABRIC8_SSO_API_URL=https://sso.prod-preview.openshift.io/
+                export FABRIC8_AUTH_API_URL=https://auth.prod-preview.openshift.io/api/
+                export PROXY_PASS_URL=https://api.free-int.openshift.com
+                npm run build
+               '''
             }
         }
     }
