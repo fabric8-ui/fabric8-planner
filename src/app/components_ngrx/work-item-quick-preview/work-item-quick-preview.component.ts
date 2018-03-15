7ded50bcfe79cf7d1659bc2f41c2e6a81f76cccf
SMahil <mahilsukh11@gmail.com>
2018-03-15 17:02:32 +0530

    fix(ngrx):fix detail and quick preview css in ngrx

--- src/app/components_ngrx/work-item-quick-preview/work-item-quick-preview.component.ts
+++ src/app/components_ngrx/work-item-quick-preview/work-item-quick-preview.component.ts
@@ -65,7 +65,7 @@ import * as LinkTypeActions from './../../actions/link-type.actions';
   animations: [
     trigger('slideInOut', [
       state('in', style({
-        transform: 'translateX(5px)',
+        transform: 'translateX(0)',
         left: 'auto'
       })),
       state('out', style({
