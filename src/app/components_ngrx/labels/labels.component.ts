899b1f4c4005159dc453911b2b8d26c3ab432d4a
Divyanshi <divyanshi@localhost.localdomain>
2018-03-26 15:41:22 +0530

    fix(dataTable) : filter by label now working

--- src/app/components_ngrx/labels/labels.component.ts
+++ src/app/components_ngrx/labels/labels.component.ts
@@ -6,7 +6,8 @@ import {
   Output
 } from '@angular/core';
 import { LabelModel, LabelUI } from './../../models/label.model';
-
+import { ActivatedRoute } from '@angular/router';
+import { FilterService } from './../../services/filter.service';
 @Component({
   selector: 'f8-label',
   templateUrl: './labels.component.html',
@@ -22,16 +23,20 @@ export class LabelsComponent {
       label.textColor
     })
   };
-
   @Input() truncateAfter: number;
   @Input() allowDelete: boolean;
+  @Input() context: string = '';
   @Output() onLabelClick = new EventEmitter();
   @Output() onRemoveLabel = new EventEmitter();
 
   private labels: LabelUI[] = [];
   private showMore: boolean = false;
+  private queryParams: any;
 
-  constructor() {}
+  constructor(
+    private route: ActivatedRoute,
+    private filterService: FilterService
+  ) {}
 
   moreClick(event) {
     event.stopPropagation();
@@ -45,5 +50,40 @@ export class LabelsComponent {
   removeLabel(label, event) {
     event.stopPropagation();
     this.onRemoveLabel.emit(label);
+  } 
+  
+  constructQueryExpression(labelId) { 
+   this.queryParams = cloneDeep(this.route.snapshot.queryParams);
+   const newQuery = this.filterService.queryBuilder(
+      'label',
+      this.filterService.equal_notation,
+      labelId
+    );
+    let existingQuery = {};
+    if (this.queryParams.hasOwnProperty('q')) {
+      existingQuery = this.filterService.queryToJson(this.queryParams['q']);
+      if(existingQuery['$AND'][existingQuery['$AND'].length-1].hasOwnProperty('label')) {
+        existingQuery['$AND'].splice(existingQuery['$AND'].length-1,1);
+      }
+    }
+    const finalQuery = this.filterService.jsonToQuery(
+      this.filterService.queryJoiner(
+        existingQuery,
+        this.filterService.and_notation,
+        newQuery
+      )
+    );
+    let queryParamObj = {};
+    if(this.queryParams.hasOwnProperty('showTree')) {
+      queryParamObj = {
+        q : finalQuery,
+        showTree : this.queryParams.showTree
+      }
+      return queryParamObj;
+    }
+    queryParamObj = {
+      q : finalQuery
+    }
+    return queryParamObj;
   }
 }
