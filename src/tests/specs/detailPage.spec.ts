a0e6731931c07a5ed67f260f4d9a6c5a6e6815ff
Raunak Bajpai <bajpairaunak@gmail.com>
2018-05-11 11:51:19 +0530

    fix(tests) : Added tests for state

--- src/tests/specs/detailPage.spec.ts
+++ src/tests/specs/detailPage.spec.ts
@@ -17,24 +17,32 @@ describe('Detail View test: ', () => {
   });
 
   beforeEach( async () => {
-    await planner.detailPage.notificationToast.untilHidden();
+    await planner.workItemList.untilUrlContains('typegroup');
+    await planner.ready();
   });
 
+  afterEach( async () => {
+    await browser.executeScript("document.getElementsByClassName('f8-detail--close')[0].click()");
+    await planner.quickPreview.notificationToast.untilHidden();
+  });
+  
   it('should open detail view and apply label', async () => {
     let workitemname = {"title": "detail page test"},
       label = 'Label1';
     await planner.createWorkItem(workitemname);
     await planner.workItemList.openDetailPage(workitemname.title);
-    // await planner.detailPage.createNewLabel(label);
     await planner.detailPage.addLabel(c.label);
     expect(await planner.detailPage.getLabels()).toContain(c.label);
   });
   
   it('should update title and description', async () => {
-    let updatedWorkItem = {
+    let workitemname = {"title": "detail page title test"},
+     updatedWorkItem = {
       title: "detail page title updated",
       description: 'New WorkItem Description'
     };
+    await planner.createWorkItem(workitemname);
+    await planner.workItemList.openDetailPage(workitemname.title);
     await planner.detailPage.updateTitle(updatedWorkItem.title);
     expect(await planner.detailPage.titleInput.getAttribute('value')).toBe(updatedWorkItem.title);
     await planner.detailPage.updateDescription(updatedWorkItem.description);
@@ -42,24 +50,39 @@ describe('Detail View test: ', () => {
   });
     
   it('Associate workitem with an Area', async () => {
+    let workitemname = {"title": "detail page test"};
+    await planner.workItemList.openDetailPage(workitemname.title);
     await planner.detailPage.addArea(c.dropdownareaTitle1);
     expect(await planner.detailPage.getArea()).toBe(c.areaTitle1);
   });
 
   it('Associate workitem with an Iteration', async () => {
+    let workitemname = {"title": "detail page test"};
+    await planner.workItemList.openDetailPage(workitemname.title);
     await planner.detailPage.addIteration(c.dropdownIteration1);
     expect(await planner.detailPage.getIteration()).toBe(c.iteration1);
   });
 
   it('should add comment', async () => {
+    let workitemname = {"title": "detail page test"};
+    await planner.workItemList.openDetailPage(workitemname.title);
     await planner.detailPage.addCommentAndSave(c.comment);
     expect(await planner.detailPage.getComments()).toContain(c.comment);
   });
 
-  it('should link a workitem',async () => {
+  it('should link a workitem', async () => {
     let linkType = 'blocks',
-      workItemTitle20 = 'Workitem_Title_20';
+      workItemTitle20 = 'Workitem_Title_20',
+      workitemname = {"title": "detail page test"};
+    await planner.workItemList.openDetailPage(workitemname.title);
     await planner.detailPage.addLink(linkType, workItemTitle20);
     expect(await planner.detailPage.getLinkedItems()).toContain(workItemTitle20);
+  });
+
+  it('should change the state of workitem', async () => {
+    let workitemname = {"title": "detail page test"};
+    await planner.workItemList.openDetailPage(workitemname.title);
+    await planner.detailPage.changeStateTo('open');
+    expect(planner.detailPage.stateToggle.getTextWhenReady()).toContain('open');
   });
 });
