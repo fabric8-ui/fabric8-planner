fd1330ce2d327de2b1b7e3c9fea09821548f2305
Vikram Raj <vikramraj1991@gmail.com>
2018-05-11 14:01:11 +0530

    fix(test): add test for initial state.

--- src/app/reducers/area.reducer.spec.ts
+++ src/app/reducers/area.reducer.spec.ts
@@ -1,5 +1,5 @@
 import { AreaReducer } from './area.reducer';
-import { initialState as AreaInitialState, AreaState } from './../states/area.state';
+import { initialState as AreaInitialState, AreaState, initialState } from './../states/area.state';
 import * as AreaActions from './../actions/area.actions';
 import { AreaUI } from './../models/area.model';
 export type Action = AreaActions.All;
@@ -10,9 +10,14 @@ describe('AreaReducer:', () => {
     const state = AreaReducer(undefined, action);
 
     expect(state).toBe(AreaInitialState);
-  })
+  });
 
-  it('Get action should return new state', () => {
+  it('Initial state should be an empty array', () => {
+    const intialState = [];
+    expect(AreaInitialState).toBe(initialState);
+  });
+
+  it('GetSuccess action should return new state', () => {
     const areas: AreaUI[] = [
       {id: '1', name: 'Area 1', parentPath: '/1223', parentPathResolved: '/space'},
       {id: '2', name: 'Area 2', parentPath: '/1224', parentPathResolved: '/space1'}
