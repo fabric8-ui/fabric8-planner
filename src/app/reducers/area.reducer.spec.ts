77337d8d89a2c179a80b21d62aa3ce84e64fcc75
Vikram Raj <vikramraj1991@gmail.com>
2018-05-11 11:56:37 +0530

    fix(test): remove extra describe from describe block

--- src/app/reducers/area.reducer.spec.ts
+++ src/app/reducers/area.reducer.spec.ts
@@ -5,39 +5,33 @@ import { AreaUI } from './../models/area.model';
 export type Action = AreaActions.All;
 
 describe('AreaReducer', () => {
-  describe('undefined actions', () => {
-    it('AreaReducer: should return the default state', () => {
-      const action = {} as Action;
-      const state = AreaReducer(undefined, action);
+  it('AreaReducer: undefined action should return the default state', () => {
+    const action = {} as Action;
+    const state = AreaReducer(undefined, action);
 
-      expect(state).toBe(AreaInitialState);
-    })
+    expect(state).toBe(AreaInitialState);
+  })
+
+  it('AreaReducer: Get action should return new state', () => {
+    const areas: AreaUI[] = [
+      {id: '1', name: 'Area 1', parentPath: '/1223', parentPathResolved: '/space'},
+      {id: '2', name: 'Area 2', parentPath: '/1224', parentPathResolved: '/space1'}
+    ];
+
+    const action = new AreaActions.GetSuccess(areas);
+    const state = AreaReducer(AreaInitialState, action);
+
+    expect(state).toEqual(areas);
   });
 
-  describe('Get actions', () => {
-    it('AreaReducer: should return new state', () => {
-      const areas: AreaUI[] = [
-        {id: '1', name: 'Area 1', parentPath: '/1223', parentPathResolved: '/space'},
-        {id: '2', name: 'Area 2', parentPath: '/1224', parentPathResolved: '/space1'}
-      ];
+  it('AreaReducer: GetError Action should return previous state', () => {
+    const previousState: AreaUI[] = [
+      {id: '1', name: 'Area 1', parentPath: '/1223', parentPathResolved: '/space'},
+      {id: '2', name: 'Area 2', parentPath: '/1224', parentPathResolved: '/space1'}
+    ];
+    const action = new AreaActions.GetError();
+    const state = AreaReducer(previousState, action);
 
-      const action = new AreaActions.GetSuccess(areas);
-      const state = AreaReducer(AreaInitialState, action);
-
-      expect(state).toEqual(areas);
-    });
-  });
-
-  describe('GetError Action', () => {
-    it('AreaReducer: should return previous state', () => {
-      const previousState: AreaUI[] = [
-        {id: '1', name: 'Area 1', parentPath: '/1223', parentPathResolved: '/space'},
-        {id: '2', name: 'Area 2', parentPath: '/1224', parentPathResolved: '/space1'}
-      ];
-      const action = new AreaActions.GetError();
-      const state = AreaReducer(previousState, action);
-
-      expect(previousState).toEqual(state);
-    });
+    expect(previousState).toEqual(state);
   });
 });
