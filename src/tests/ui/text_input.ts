f74b2751caa159b8a4803d73a8f30e0922371b6d
sahil143 <sahilbudhwar143@gmail.com>
2018-06-21 14:33:17 +0530

    fix(test): add test to create a label using enter key

--- src/tests/ui/text_input.ts
+++ src/tests/ui/text_input.ts
@@ -1,5 +1,6 @@
 import { ExpectedConditions as until, ElementFinder } from 'protractor';
 import { BaseElement } from './base.element';
+import { protractor } from 'protractor/built/ptor';
 
 export class TextInput extends BaseElement {
 
@@ -8,11 +9,18 @@ export class TextInput extends BaseElement {
   }
 
   async enterText(text: string) {
-    await this.run('enter text', async()=> {
+    await this.run('enter text', async () => {
       await this.ready();
       await this.sendKeys(text);
-    })
+    });
     this.log('Entered Text');
+  }
+
+  async pressEnter() {
+    await this.run('press enter', async () => {
+      await this.ready();
+      await this.sendKeys(protractor.Key.ENTER);
+    });
   }
 }
 
