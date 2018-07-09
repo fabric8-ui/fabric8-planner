5a81ca96be756761f2dcc24863a1dfb55b853696
sahil143 <sahilbudhwar143@gmail.com>
2018-07-09 12:19:30 +0530

    fix(board): add states and reducer to board module

--- src/app/components_ngrx/planner-board/planner-board.module.ts
+++ src/app/components_ngrx/planner-board/planner-board.module.ts
@@ -1,18 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { BoardEffects } from '../../effects/board.effect';
+import { BoardReducer, ColumnWorkItemReducer } from '../../reducers/index.reducer';
+import { InitialBoardState, InitialColumnWorkItemState } from './../../states/index.state';
 import { PlannerBoardRoutingModule } from './planner-board-routing.module';
 import { PlannerBoardComponent } from './planner-board.component';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        PlannerBoardRoutingModule
-    ],
-    declarations: [
-        PlannerBoardComponent
-    ],
-    exports: [
-        PlannerBoardComponent
-    ]
+  imports: [
+    CommonModule,
+    PlannerBoardRoutingModule,
+    StoreModule.forFeature('boardView', {
+      boards: BoardReducer,
+      columnWorkItem: ColumnWorkItemReducer
+    }, {
+      initialState: {
+        boards: InitialBoardState,
+        columnWorkItem: InitialColumnWorkItemState
+      }
+    }),
+    EffectsModule.forFeature([
+      BoardEffects
+    ])
+  ],
+  declarations: [PlannerBoardComponent],
+  exports: [PlannerBoardComponent]
 })
 export class PlannerBoardModule {}
