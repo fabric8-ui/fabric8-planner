58bb0496835f3f70a26b40bff50eca7ae1da7064
sahil143 <sahilbudhwar143@gmail.com>
2018-07-10 11:26:25 +0530

    fix(board): assign value in board effects, change type of columnIds, fix unit tests, fix column-workItem reducer

--- src/app/reducers/column-workitem.reducer.ts
+++ src/app/reducers/column-workitem.reducer.ts
@@ -2,26 +2,26 @@ import { ActionReducer } from '@ngrx/store';
 import * as  WorkItemActions  from '../actions/work-item.actions';
 import { ColumnWorkItemState, InitialColumnWorkItemState } from '../states/index.state';
 
+
 export type Action = WorkItemActions.All;
 
 export const ColumnWorkItemReducer: ActionReducer<ColumnWorkItemState> = (state = InitialColumnWorkItemState, action: Action) => {
   switch (action.type) {
     case WorkItemActions.GET_SUCCESS: {
+      const cwState = {...state};
       action.payload.forEach(item => {
-        if (item.columnsId === null) {
-          return state;
-        } else {
-          item.columnsId.forEach(col => {
-            if (state.hasOwnProperty(col.id)) {
-              state[col.id].push(item.id);
+        if (item.columnIds !== null) {
+          item.columnIds.forEach(col => {
+            if (cwState.hasOwnProperty(col)) {
+              cwState[col] = [...cwState[col], item.id];
             } else {
-              state[col.id] = [];
-              state[col.id].push(item.id);
+              cwState[col] = [];
+              cwState[col] = [...cwState[col], item.id];
             }
           });
         }
       });
-      return {...state};
+      return {...cwState};
     }
   }
 };
