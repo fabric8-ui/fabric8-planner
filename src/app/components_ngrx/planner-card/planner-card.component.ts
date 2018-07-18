8b0caa04cb889fd6082c6a6d5f7c2b07d9bdaaab
Vikram Raj <vikramraj1991@gmail.com>
2018-07-18 13:31:34 +0530

    fix(card): add assignee component

--- src/app/components_ngrx/planner-card/planner-card.component.ts
+++ src/app/components_ngrx/planner-card/planner-card.component.ts
@@ -19,43 +19,6 @@ export class PlannerCardComponent {
     titleBorder: false
   };
 
-  private labels = [{
-    backgroundColor: '#d1d1d1',
-    borderColor: '#8461f7',
-    id: '22fdcbda-6556-4940-8637-8af834499271',
-    name: 'Area/Planner',
-    textColor: '#000000',
-    version: 0
-  }, {
-    backgroundColor: '#a18fff',
-    borderColor: '#bbbbbb',
-    id: '2698b84a-c121-47b2-8ff5-28a76c808958',
-    name: 'Important',
-    textColor: '#000000',
-    version: 0
-  }, {
-    backgroundColor: '#fbeabc',
-    borderColor: '#f39d3c',
-    id: '197725a8-1cd4-4e25-a763-b4a2747fc92b',
-    name: 'label 9',
-    textColor: '#000000',
-    version: 0
-  }, {
-    backgroundColor: '#a18fff',
-    borderColor: '#bbbbbb',
-    id: '2698b84a-c121-47b2-8ff5-28a76c808958',
-    name: 'Important',
-    textColor: '#000000',
-    version: 0
-  }, {
-    backgroundColor: '#fbeabc',
-    borderColor: '#f39d3c',
-    id: '197725a8-1cd4-4e25-a763-b4a2747fc92b',
-    name: 'label 9',
-    textColor: '#000000',
-    version: 0
-  }];
-
   cardClick(workItem: WorkItemUI, event) {
     event.stopPropagation();
     this.onCardClick.emit(workItem);
