7ae42ad491e12e34da776569817c41e7e2522259
Raunak Bajpai <bajpairaunak@gmail.com>
2018-10-24 12:26:56 +0530

    Add tests for creating a new workitem from query tab

--- src/tests/specs/query-tab.spec.ts
+++ src/tests/specs/query-tab.spec.ts
@@ -23,4 +23,16 @@ describe('Query tests', () => {
     await planner.query.enterQuery(searchQuery);
     expect(await planner.query.getDataTableHeaderCellCount()).toBe(8);
   });
+
+  it('should create a new Workitem', async () => {
+    let title: string = 'Create work item from query page',
+      searchQuery: string = `title:${title}`;
+    await planner.clickQueryTab();
+    await planner.waitUntilUrlContains('query');
+    await planner.query.createWorkItem(title);
+    await planner.quickPreview.notificationToast.untilCount(1);
+    expect(await planner.quickPreview.notificationToast.count()).toBe(1);
+    await planner.query.enterQuery(searchQuery);
+    expect(await planner.query.hasWorkItem(title)).toBe(true);
+  });
 });
